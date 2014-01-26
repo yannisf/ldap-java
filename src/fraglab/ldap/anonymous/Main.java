744665aca8117903fd03d4a2cddcbfbd610955e7
yannisf <yannis@ianos>
2014-01-26 23:39:52 +0200

    Initial commit

---
+++ src/fraglab/ldap/anonymous/Main.java
@@ -0,0 +1,20 @@
+package fraglab.ldap.anonymous;
+
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import java.util.Hashtable;
+
+public class Main {
+
+    public static void main(String[] args) throws NamingException {
+        Hashtable<String, Object> env = new Hashtable<String, Object>();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        env.put(Context.PROVIDER_URL, "ldap://localhost:1389/dc=aux-echa,dc=es,dc=atos,dc=net");
+        env.put(Context.SECURITY_AUTHENTICATION, "none");
+        DirContext ctx = new InitialDirContext(env);
+        ctx.close();
+    }
+
+}
---
+++ src/fraglab/ldap/simple/Main.java
@@ -0,0 +1,36 @@
+package fraglab.ldap.simple;
+
+import javax.naming.Context;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import java.util.Hashtable;
+
+public class Main {
+
+    public static void main(String[] args) throws NamingException {
+        Hashtable<String, Object> env = new Hashtable<String, Object>();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        env.put(Context.PROVIDER_URL, "ldap://localhost:1389/dc=aux-echa,dc=es,dc=atos,dc=net");
+        env.put(Context.SECURITY_AUTHENTICATION, "simple");
+        env.put(Context.SECURITY_PRINCIPAL, "cn=admin,dc=aux-echa,dc=es,dc=atos,dc=net");
+        env.put(Context.SECURITY_CREDENTIALS, "yannis");
+        DirContext ctx = new InitialDirContext(env);
+        SearchControls searchControls = new SearchControls();
+        searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+        searchControls.setCountLimit(100);
+        NamingEnumeration<SearchResult> namingEnumeration =
+                ctx.search("", "(uid=*)", new Object[]{}, searchControls);
+        while (namingEnumeration.hasMore()) {
+            SearchResult sr = namingEnumeration.next();
+            System.out.println("DN: " + sr.getName());
+            System.out.println(sr.getAttributes().get("uid"));
+            System.out.println("Password:" + new String((byte[]) sr.getAttributes().get("userPassword").get()));
+        }
+        ctx.close();
+    }
+
+}
